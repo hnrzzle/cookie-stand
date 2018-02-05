9f5733b2b86520ed2087c9011c8746275846c64a
hnrzzle <henrique.j.oliveira1@gmail.com>
2018-02-05 15:37:34 -0800

    added methods, working on arrays

--- sales.js
+++ sales.js
@@ -11,5 +11,39 @@
 const pdx = {
     minCust: 23,
     maxCust: 65,
-    
-}
+    avgCookies: 6.3,
+    randoCust: function() {
+        min = Math.ceil(this.minCust);
+        max = Math.floor(this.maxCust);
+        let random = Math.floor(Math.random() * (max - min +1)) + min;
+        return random;
+    },
+    simCookies: function() {
+        let sim = this.avgCookies * this.randoCust();
+        return sim;
+    },
+    pdxProperties: [
+        this.minCust,
+        this.maxCust,
+        this.avgCookies,
+        this.randoCust,
+        this.simCookies
+        ],
+    createLi: function() {
+        const li = document.createElement('li');
+        li.textContent = 'blah';
+        return li;
+    }
+};
+pdx.createLi();
+
+// const pdxProperties = [
+//                     pdx.minCust,
+//                     pdx.maxCust,
+//                     pdx.avgCookies,
+//                     pdx.randoCust(),
+//                     pdx.simCookies()
+//                     ];
+
+// const newList = document.createElement('li')
+// li.textContent = this.
