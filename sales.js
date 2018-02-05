bfc7f7802e17dd264bd68cfbbd8b57aaec7ef835
hnrzzle <henrique.j.oliveira1@gmail.com>
2018-02-05 13:51:22 -0800

    outlined project goals and started sales.js

---
+++ sales.js
@@ -0,0 +1,15 @@
+/* Create a seperate JS object literal for each shop that:
+-Stores min/max hourly customers
+-"            " average cookies per customer, in object properties
+-Uses a method of that object to generate a random number of customers per hour use Math.random() method
+-Calculate and store The simulated amounts of cookies purchased for each hour at each location using average cookies purchased and random number of customers generated
+-Store the results for each location in a seperate array... as a property of the object representing that location
+-Display the values of each array as <ol> in the browser
+*/
+
+
+const pdx = {
+    minCust: 23,
+    maxCust: 65,
+    
+}
