db4aa24918ed0c7e801a66ed7bb49657e3e59dec
hnrzzle <henrique.j.oliveira1@gmail.com>
2018-02-07 13:49:35 -0800

    successfully building tables, need to move totals to footer potentially

--- sales.js
+++ sales.js
@@ -7,10 +7,11 @@
 -Display the values of each array as <ol> in the browser
 */
 // time array
-const time = ['6am: ', '7am: ', '8am: ', '9am: ', '10am: ', '11am: ', '12pm: ', '1pm: ', '2pm: ', '3pm: ', '4pm: ', '5pm: ', '6pm: ', '7pm: ', '8pm: ', 'total: ' ];
+const time = ['6AM', '7AM', '8AM', '9AM', '10AM', '11AM', '12PM', '1PM', '2PM', '3PM', '4PM', '5PM', '6PM', '7PM', '8PM', 'Total'];
 
 // constructor function
-function Location(minCust, maxCust, avgCookies) {
+function Location(name, minCust, maxCust, avgCookies) {
+    this.name = name;
     this.minCust = minCust;
     this.maxCust = maxCust;
     this.avgCookies = avgCookies;
@@ -33,6 +34,7 @@ Location.prototype.getTotalCookies = function() {
     }
     this.hourlyArray.push(total);
 };
+/*
 Location.prototype.renderCookieAmt = function(location) {
     for (let i = 0; i < this.hourlyArray.length; i++) {
         const list = document.getElementById(location);
@@ -41,33 +43,68 @@ Location.prototype.renderCookieAmt = function(location) {
         list.appendChild(li);
     }
 };
+*/
+
+Location.prototype.renderRow = function() {
+    this.cookiesHour();
+    this.getTotalCookies();
+    //Create first row with location name
+    const tbody = document.getElementById('tbody');
+    const tr = document.createElement('tr');
+    const td = document.createElement('td');
+    td.textContent = this.name;
+    tr.appendChild(td);
+    for (let i = 0; i < this.hourlyArray.length; i++) {
+        const td = document.createElement('td');
+        td.textContent = this.hourlyArray[i];
+        tr.appendChild(td);
+        tbody.appendChild(tr);
+    }
+};
 
 
-const pdx = new Location(23, 65, 6.3);
-const pioneerSquare = new Location(3, 24, 1.2);
-const powells = new Location(11, 38, 3.7);
-const stJohns = new Location(20, 38, 2.3);
-const waterfront = new Location(2, 16, 4.6);
+function buildHeader () {
+    for (let i = 0; i < time.length; i++) {
+        const header = document.getElementById('header-row');
+        const th = document.createElement('th');
+        th.textContent = time[i];
+        header.appendChild(th);
+    }
+};
+
+
+buildHeader();
+const pdx = new Location('PDX Airport', 23, 65, 6.3);
+const pioneerSquare = new Location('Pioneer Square', 3, 24, 1.2);
+const powells = new Location('Powell\'s', 11, 38, 3.7);
+const stJohns = new Location('St. John\'s', 20, 38, 2.3);
+const waterfront = new Location('Waterfront', 2, 16, 4.6);
 
 
 // method calls and logs
-pdx.cookiesHour();
-pdx.getTotalCookies();
-pdx.renderCookieAmt('airport');
-console.log(pdx.hourlyArray);
-pioneerSquare.cookiesHour();
-pioneerSquare.getTotalCookies();
-pioneerSquare.renderCookieAmt('pioneer');
-console.log(pioneerSquare.hourlyArray);
-powells.cookiesHour();
-powells.getTotalCookies();
-powells.renderCookieAmt('powells');
-console.log(powells.hourlyArray);
-stJohns.cookiesHour();
-stJohns.getTotalCookies();
-stJohns.renderCookieAmt('st-johns');
-console.log(stJohns.hourlyArray);
-waterfront.cookiesHour();
-waterfront.getTotalCookies();
-waterfront.renderCookieAmt('waterfront');
-console.log(waterfront.hourlyArray);
+pdx.renderRow();
+pioneerSquare.renderRow();
+powells.renderRow();
+stJohns.renderRow();
+waterfront.renderRow();
+
+// pdx.renderCookieAmt('airport');
+// console.log(pdx.hourlyArray);
+// pioneerSquare.cookiesHour();
+// pioneerSquare.getTotalCookies();
+// pioneerSquare.renderCookieAmt('pioneer');
+// console.log(pioneerSquare.hourlyArray);
+// powells.cookiesHour();
+// powells.getTotalCookies();
+// powells.renderCookieAmt('powells');
+// console.log(powells.hourlyArray);
+// stJohns.cookiesHour();
+// stJohns.getTotalCookies();
+// stJohns.renderCookieAmt('st-johns');
+// console.log(stJohns.hourlyArray);
+// waterfront.cookiesHour();
+// waterfront.getTotalCookies();
+// waterfront.renderCookieAmt('waterfront');
+// console.log(waterfront.hourlyArray);
+
+
